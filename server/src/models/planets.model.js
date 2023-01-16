9924ae9fac0a70f80fd268b0c6a025e9a110c83c
Rulon <giancalonghi@hotmail.com>
2023-01-15 23:10:52 -0600

    Add kepler planet data and filter out uninhabitable planets

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,34 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(parse({ 
+    comment: '#',
+    columns: true,
+  }))
+  .on('data', (chunk) => {
+    if (isHabitablePlanet(chunk)) {
+      habitablePlanets.push(chunk);
+    }
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(habitablePlanets.map(planet => {
+      return planet['kepler_name'];
+    }));
+    console.log(`We found ${habitablePlanets.length} habitable planets!`);
+  });
+
+module.exports = {
+  planets: habitablePlanets
+};
