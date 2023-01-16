a354333265c36a308baa12aaf0ed1984e47986b7
Rulon <giancalonghi@hotmail.com>
2023-01-16 11:54:45 -0600

    Fix bug causing imports to fail

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { getAllPlanets } = require('./planets.controller');
+const getAllPlanets = require('./planets.controller');
 
 const planetsRouter = express.Router();
 
