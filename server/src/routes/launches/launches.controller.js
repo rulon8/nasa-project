adef70b8b6d0be191e2bd3d67cfd9c84e1021e73
Rulon <giancalonghi@hotmail.com>
2023-01-17 15:10:03 -0600

    Add post route and controller action for adding new launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,22 @@
-const { getLaunches } = require('../../models/launches.model');
+const { 
+  getLaunches,
+  addLaunch,
+} = require('../../models/launches.model');
 
 function getAllLaunches(request, response) {
   return response.status(200).json(getLaunches());
 }
 
+function addNewLaunch(request, response) {
+  const launch = request.body;
+
+  launch.launchDate = new Date(launch.launchDate);
+
+  addLaunch(launch);
+  return response.status(201).json(launch);
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch
 };
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
-const { getAllLaunches } = require('./launches.controller');
+const { 
+  getAllLaunches,
+  addNewLaunch
+} = require('./launches.controller');
 
 const launchesRouter = express.Router();
 
 launchesRouter.get('/launches', getAllLaunches);
+launchesRouter.post('/launches', addNewLaunch);
 
 module.exports = launchesRouter;
