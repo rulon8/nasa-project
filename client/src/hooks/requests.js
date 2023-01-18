549a1491c0c4e98442183f51481bc06775fe1db2
Rulon <giancalonghi@hotmail.com>
2023-01-17 19:21:31 -0600

    Add call to delete launch endpoint to abort launches

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -32,9 +32,17 @@ async function httpSubmitLaunch(launch) {
   }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    return await fetch(`${URL}/launches/${id}`, {
+      method: 'delete',
+    });
+  } catch (err) {
+    return {
+      ok: false
+    };
+  }
 }
 
 export {
