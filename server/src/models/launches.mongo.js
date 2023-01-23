b8e14a0041a091b1bd02742fa4cc56743ef18bf6
Rulon <giancalonghi@hotmail.com>
2023-01-22 19:49:59 -0600

    Create mongoose schema and model for launches

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+// Creates a model from the schema. The model connects the schema to the
+// launches collection. A model's name must be singular.
+const launchesModel = mongoose.model('Launch', launchesSchema);
+
+module.exports = launchesModel;
