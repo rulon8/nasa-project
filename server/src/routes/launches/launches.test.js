aaab5539ef9b91f30ab6f1066a57845b40f94e23
Rulon <giancalonghi@hotmail.com>
2023-01-24 11:31:28 -0600

    Fix variable name bugs in test cases

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -8,72 +8,72 @@ describe('Launches API', () => {
     await mongoConnect();
   });
 
-describe('Test GET /launches', () => {
-  test('It should respond with 200 success', async () => {
-    const response = await request(app)
-      .get('/launches')
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-});
-
-describe('Test POST /launches', () => {
-  const completeLaunch = {
-    mission: 'USS Enterprise',
-    rocket: 'NCC-1701-D',
-    target: 'Kepler-442 b',
-    launchDate: 'January 4, 2028',
-  };
-
-  const launchWithoutDate = {
-    mission: 'USS Enterprise',
-    rocket: 'NCC-1701-D',
-    target: 'Kepler-442 b',
-  };
-
-  const launchWithInvalidDate = {
-    mission: 'USS Enterprise',
-    rocket: 'NCC-1701-D',
-    target: 'Kepler-442 b',
-    launchDate: 'zoot',
-  };
-
-  test('It should respond with 201 created', async () => {
-    const response = await request(app)
-      .post('/launches')
-      .send(completeLaunch)
-      .expect('Content-Type', /json/)
-      .expect(201);
-
-    const requestDate = new Date(completeLaunch.launchDate).valueOf();
-    const responseDate = new Date(response.body.launchDate).valueOf();
-    expect(responseDate).toBe(requestDate);
-
-    expect(response.body).toMatchObject(launchWithoutDate);
-  });
-
-  test('It should catch missing required properties', async () => {
-    const response = await request(app)
-      .post('/launches')
-      .send(launchDataWithoutDate)
-      .expect('Content-Type', /json/)
-      .expect(400);
-
-    expect(response.body).toStrictEqual({
-      error: 'Missing required launch property',
+  describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async () => {
+      const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', /json/)
+        .expect(200);
     });
   });
-
-  test('It should catch invalid dates', async () => {
-    const response = await request(app)
-      .post('/launches')
-      .send(launchDataWithInvalidDate)
-      .expect('Content-Type', /json/)
-      .expect(400);
-
-    expect(response.body).toStrictEqual({
-      error: 'Invalid launch date',
+  
+  describe('Test POST /launches', () => {
+    const completeLaunch = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC-1701-D',
+      target: 'Kepler-442 b',
+      launchDate: 'January 4, 2028',
+    };
+  
+    const launchWithoutDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC-1701-D',
+      target: 'Kepler-442 b',
+    };
+  
+    const launchWithInvalidDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC-1701-D',
+      target: 'Kepler-442 b',
+      launchDate: 'zoot',
+    };
+  
+    test('It should respond with 201 created', async () => {
+      const response = await request(app)
+        .post('/launches')
+        .send(completeLaunch)
+        .expect('Content-Type', /json/)
+        .expect(201);
+  
+      const requestDate = new Date(completeLaunch.launchDate).valueOf();
+      const responseDate = new Date(response.body.launchDate).valueOf();
+      expect(responseDate).toBe(requestDate);
+  
+      expect(response.body).toMatchObject(launchWithoutDate);
+    });
+  
+    test('It should catch missing required properties', async () => {
+      const response = await request(app)
+        .post('/launches')
+        .send(launchWithoutDate)
+        .expect('Content-Type', /json/)
+        .expect(400);
+  
+      expect(response.body).toStrictEqual({
+        error: 'Missing required launch property',
+      });
+    });
+  
+    test('It should catch invalid dates', async () => {
+      const response = await request(app)
+        .post('/launches')
+        .send(launchWithInvalidDate)
+        .expect('Content-Type', /json/)
+        .expect(400);
+  
+      expect(response.body).toStrictEqual({
+        error: 'Invalid launch date',
+      });
     });
   });
-});
 });
