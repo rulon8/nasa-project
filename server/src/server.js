42b8ab06c4c534829979b9c827d802933ca393fd
Rulon <giancalonghi@hotmail.com>
2023-01-24 11:29:22 -0600

    Connect to mongoDB as soon as the server starts

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const app = require('./app');
+
+const mongoConnect = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
@@ -8,6 +10,9 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 async function startServer() {
+  // Connect to MongoDB.
+  await mongoConnect();
+
   // Wait for planet data to load from CSV file.
   await loadPlanetsData();
 
